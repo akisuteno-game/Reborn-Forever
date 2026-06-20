81b149f4116e6a6f815e177e0c0c60264d20261a
akisuteno-game <akisuteno5160@gmail.com>
2026-06-20 19:44:20 +0900

    Update engine.js

--- js/core/engine.js
+++ js/core/engine.js
@@ -1,19 +1,19 @@
-// js/core/engine.js
 export class Engine {
-  constructor(state) {
-    this.state = state;
-    this.systems = {};
+  constructor(eventBus) {
+    this.bus = eventBus;
+    this.state = {
+      player: null,
+      time: 0
+    };
   }
 
-  register(name, system) {
-    this.systems[name] = system;
-    if (system.init) system.init(this);
+  setPlayer(playerData) {
+    this.state.player = playerData;
+    this.bus.emit("player:loaded", playerData);
   }
 
-  tick(delta = 1) {
-    // delta は秒単位だがここでは1tickごとに1として扱う
-    for (const s of Object.values(this.systems)) {
-      if (s.update) s.update(delta, this.state, this);
-    }
+  tick() {
+    this.state.time++;
+    this.bus.emit("engine:tick", { time: this.state.time });
   }
 }
